d83ec34bc39a2a316c95743f3848cef6f39377d6
fpumir <fpumir@terredeweb.fr>
2014-10-17 16:49:54 +0200

    final volume
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- video/js/video.js
+++ video/js/video.js
@@ -4,14 +4,19 @@ var video=document.getElementById('video');
 var button=document.getElementById('button');
 var progressBar=document.getElementById('progressBar');
 var autoPlayed=false;//on créé une variable d'état à false
+var volumePlus=document.querySelector('.plus');
+var volumeMoins=document.querySelector('.moins');
 
 video.load();
 button.classList.add('loading');
 
 window.addEventListener('click',playPause,false);
+window.addEventListener('keypress',playPause,false);
 video.addEventListener('canplaythrough',autoPlay,false);
 video.addEventListener('timeupdate',progress,false);
 progressBar.addEventListener('click',setVideoTime,false);
+volumePlus.addEventListener('click',setMoreVolume,false);
+volumeMoins.addEventListener('click',setLessVolume,false);
 
 
 function autoPlay(e){
@@ -23,7 +28,10 @@ function autoPlay(e){
 }
 
 function playPause(e){
-	console.log(e); 
+	if(e.keyIdentifier){
+		if(e.keyCode!=32){return;}
+	}
+		e.preventDefault();
 		if(video.paused){
 			video.play();
 			button.classList.add('play');
@@ -45,6 +53,32 @@ function setVideoTime (e){
 	video.currentTime=time;
 }
 
+function setMoreVolume(e){
+	e.stopPropagation();
+	if(volumeMoins.classList.contains('off')){
+		volumeMoins.classList.remove('off');
+	}
+	if(video.volume<1){
+		video.volume+=0.1;
+	}	
+	else{
+		this.classList.add('off');
+	}
+}
+
+function setLessVolume(e) {
+	e.stopPropagation();
+	if(volumePlus.classList.contains('off')){
+		volumePlus.classList.remove('off');
+	}
+	if(video.volume>0.1){
+		video.volume-=0.1;
+	}
+	else{
+		this.classList.add('off');
+	}
+	
+}
 
 
 
